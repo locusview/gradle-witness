518bb43ded56e91c95fe93e970cf2f06a930f63e
akwizgran <michael@briarproject.org>
2017-11-17 11:32:14 +0000

    Verify dependencies for all resolvable configurations.

--- src/main/groovy/org/whispersystems/witness/WitnessPlugin.groovy
+++ src/main/groovy/org/whispersystems/witness/WitnessPlugin.groovy
@@ -24,38 +24,57 @@ class WitnessPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.extensions.create("dependencyVerification", WitnessPluginExtension)
         project.afterEvaluate {
+            List artifacts = new ArrayList()
+            project.configurations.each {
+                if (it.metaClass.respondsTo(it, 'isCanBeResolved') ? it.isCanBeResolved() : true) {
+                    artifacts.addAll(it.resolvedConfiguration.resolvedArtifacts)
+                }
+            }
             project.dependencyVerification.verify.each {
                 assertion ->
-                    List  parts  = assertion.tokenize(":")
-                    String group = parts.get(0)
-                    String name  = parts.get(1)
-                    String hash  = parts.get(2)
+                    List parts = assertion.tokenize(":")
+                    if (parts.size() != 5) {
+                        throw new InvalidUserDataException("Invalid or obsolete integrity assertion '" + assertion + "'")
+                    }
+                    def (group, name, version, file, hash) = parts
 
-                    ResolvedArtifact dependency = project.configurations.compile.resolvedConfiguration.resolvedArtifacts.find {
-                        return it.name.equals(name) && it.moduleVersion.id.group.equals(group)
+                    List dependencies = artifacts.findAll {
+                        return it.name.equals(name) && it.moduleVersion.id.group.equals(group) && it.moduleVersion.id.version.equals(version) && it.file.name.equals(file)
                     }
 
-                    println "Verifying " + group + ":" + name
+                    println "Verifying " + group + ":" + name + ":" + version + ":" + file
 
-                    if (dependency == null) {
-                        throw new InvalidUserDataException("No dependency for integrity assertion found: " + group + ":" + name)
+                    if (dependencies.isEmpty()) {
+                        throw new InvalidUserDataException("No dependency for integrity assertion found: " + group + ":" + name + ":" + version + ":" + file)
                     }
 
-                    if (!hash.equals(calculateSha256(dependency.file))) {
-                        throw new InvalidUserDataException("Checksum failed for " + assertion)
+                    Set files = new TreeSet()
+                    dependencies.each { files.add(it.file) }
+                    files.each {
+                        if (!hash.equals(calculateSha256(it))) {
+                            throw new InvalidUserDataException("Checksum failed for " + assertion)
+                        }
                     }
             }
         }
 
         project.task('calculateChecksums') << {
-            println "dependencyVerification {"
-            println "    verify = ["
-
-            project.configurations.compile.resolvedConfiguration.resolvedArtifacts.each {
-                dep ->
-                    println "        '" + dep.moduleVersion.id.group+ ":" + dep.name + ":" + calculateSha256(dep.file) + "',"
+            Set dependencies = new TreeSet()
+            project.configurations.each {
+                if (it.metaClass.respondsTo(it, 'isCanBeResolved') ? it.isCanBeResolved() : true) {
+                    it.resolvedConfiguration.resolvedArtifacts.findAll {
+                        // Skip internal dependencies
+                        it.moduleVersion.id.version != 'unspecified'
+                    }.each {
+                        dep ->
+                          dependencies.add(dep.moduleVersion.id.group + ":" + dep.name + ":" + dep.moduleVersion.id.version + ":" + dep.file.name + ":" + calculateSha256(dep.file))
+                    }
+                }
             }
 
+            println "dependencyVerification {"
+            println "    verify = ["
+            dependencies.each { dep -> println "        '" + dep + "'," }
             println "    ]"
             println "}"
         }
